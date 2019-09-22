7b02554b686ca233b90fdf8743f3bdfcf8d7fcc6
N-KAVYA <37626891+N-KAVYA@users.noreply.github.com>
2019-09-22 19:01:43 +0530

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,11 @@
+print("code for problem5")
+
+def reve(sen):
+    word=sen.split(" ")
+    word=word[-1::-1]
+    y=' '.join(word)
+    return y
+if __name__ == "__main__":
+    sen=input("enter the sentence: ")
+    sen=str(sen)
+    print((reve(sen)).capitalize())
